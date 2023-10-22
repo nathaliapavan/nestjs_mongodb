7f88812b8fae4d21a56a41e512434b068f8bc471
nathaliapavan <nanahpavan@gmail.com>
2023-10-22 16:20:11 -0300

    feature: add redis to project

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongoDBService } from '../../infra/database/mongodb/mongodb.service';
+import { CacheService } from './../../infra/database/redis/cache.service';
+import { RedisService } from './../../infra/database/redis/redis.service';
 
 @Module({
-  providers: [UserService, MongoDBService],
+  providers: [UserService, MongoDBService, RedisService, CacheService],
   controllers: [UserController],
 })
 export class UserModule {}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,11 +4,16 @@ import { Model } from 'mongoose';
 import { CreateUserInput, UpdateUserInput } from './model/user.input';
 import { UserPayload } from './model/user.payload';
 import { MongoDBService } from '../../infra/database/mongodb/mongodb.service';
+import { CacheService } from '../../infra/database/redis/cache.service';
 
 @Injectable()
 export class UserService {
   private userModel: Model<User>;
-  constructor(private readonly mongoDBService: MongoDBService) {
+
+  constructor(
+    private readonly mongoDBService: MongoDBService,
+    private readonly cacheService: CacheService,
+  ) {
     this.userModel = this.mongoDBService
       .getMongooseInstance()
       .model(User.name, UserSchema);
@@ -17,16 +22,23 @@ export class UserService {
   async createUser(body: CreateUserInput): Promise<UserPayload> {
     const createdUser = new this.userModel(body);
     const user = await createdUser.save();
+    await this.cacheService.set(`user:${user.id}`, JSON.stringify(user));
     return user;
   }
 
   async findUser(id: string): Promise<UserPayload> {
-    const user = await this.userModel.findOne({ _id: id }).exec();
-
-    if (!user) {
-      throw new NotFoundException(`User with email id:${id} not found `);
+    try {
+      const cachedUser = await this.cacheService.get(`user:${id}`);
+      if (cachedUser) {
+        const user = JSON.parse(cachedUser);
+        console.log(`${user._id} from Redis`);
+        return user;
+      }
+      return this.getUserByMongoDB(id);
+    } catch (error) {
+      console.error('Error accessing Redis:', error.message);
+      return this.getUserByMongoDB(id);
     }
-    return user;
   }
 
   async listUser(): Promise<UserPayload[]> {
@@ -36,11 +48,24 @@ export class UserService {
 
   async updateUser(id: string, body: UpdateUserInput): Promise<UserPayload> {
     await this.userModel.updateOne({ _id: id }, body);
-    const updatedUser = this.userModel.findById(id);
+    const updatedUser = await this.userModel.findById(id);
+    if (updatedUser) {
+      await this.cacheService.set(`user:${id}`, JSON.stringify(updatedUser));
+    }
     return updatedUser;
   }
 
   async deleteUser(id: string): Promise<void> {
+    await this.cacheService.delete(`user:${id}`);
     await this.userModel.deleteOne({ _id: id });
+  }
+
+  private async getUserByMongoDB(id: string) {
+    const user = await this.userModel.findOne({ _id: id }).exec();
+    if (!user) {
+      throw new NotFoundException(`User with id:${id} not found `);
+    }
+    console.log(`${user.id} from MongoDB`);
+    return user;
   }
 }
