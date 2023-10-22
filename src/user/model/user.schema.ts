171e76f764661691c3c759b89275720222bb1cfc
nathaliapavan <nanahpavan@gmail.com>
2023-10-22 12:07:24 -0300

    style: apply prettier

--- src/user/model/user.schema.ts
+++ src/user/model/user.schema.ts
@@ -1,21 +1,21 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { HydratedDocument } from 'mongoose'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 
-export type UserDocument = HydratedDocument<User>
+export type UserDocument = HydratedDocument<User>;
 
 @Schema({ collection: 'users', timestamps: true })
 export class User {
   @Prop()
-  fullName: string
+  fullName: string;
 
   @Prop()
-  email: string
+  email: string;
 
   @Prop()
-  bio: string
+  bio: string;
 
   @Prop()
-  password: string
+  password: string;
 }
 
-export const UserSchema = SchemaFactory.createForClass(User)
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common'
-import { UserService } from './user.service'
-import { UserController } from './user.controller'
-import { MongooseModule } from '@nestjs/mongoose'
-import { User, UserSchema } from './model/user.schema'
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './model/user.schema';
 
 @Module({
   imports: [
